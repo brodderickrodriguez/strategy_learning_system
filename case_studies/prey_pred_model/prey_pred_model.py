87ab183e264125d6982f53f1c835417a7b36dd8b
brodderickrodriguez <bcr@brodderick.com>
2019-08-27 15:44:11 -0500

    Update prey_pred_model.py

--- case_studies/prey_pred_model/prey_pred_model.py
+++ case_studies/prey_pred_model/prey_pred_model.py
@@ -4,16 +4,21 @@
 
 import strategy_learning_system as sls
 
+
 def define_problem():
 	inter = sls.Interpreter(name='prey_pred')
 
-	model_dir = '/Users/bcr/Dropbox/Projects/CODE/netlogo/prey_predator_nlogo'
+	root = '/Users/bcr/Dropbox/Projects/CODE'
+
+	model_dir = root + '/netlogo/prey_predator_nlogo'
 	model_name = 'preypred.nlogo'
 	inter.model = (model_dir, model_name)
 
-
+	inter.save_location = root + '/Python/strategy_learning_system/case_studies/prey_pred_model'
 	return inter
 
 
+
 interpreter = define_problem()
+interpreter.save()
 print(interpreter)
