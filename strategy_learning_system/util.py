b3f27eeeeaf2fd21c37ced4cd293e6490fd36615
brodderickrodriguez <bcr@brodderick.com>
2019-08-27 15:00:51 -0500

    Update util.py

--- strategy_learning_system/util.py
+++ strategy_learning_system/util.py
@@ -4,3 +4,17 @@
 
 
 # TODO: I/O
+
+import ema_workbench
+import numpy as np
+
+
+def load_results(data_dir):
+    # use EMA's built-in load_results function to unzip and load experiment results
+    exp_df, out_dict = ema_workbench.load_results(data_dir)
+
+    # remove unnecessary columns
+    exp_df = exp_df.drop(['policy', 'model', 'scenario'], axis=1)
+
+    # return tuple containing the experiments and outcomes 
+    return exp_df, out_dict
