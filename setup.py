7aef1228b14dd732a5f8d2a51eefdea63119cf91
brodderickrodriguez <bcr@brodderick.com>
2019-08-27 15:00:10 -0500

    created package

---
+++ setup.py
@@ -0,0 +1,16 @@
+# Brodderick Rodriguez
+# Auburn University - CSSE
+# 27 Aug. 2019
+
+from setuptools import setup
+
+
+setup(name='strategy_learning_system',
+      version='0.1',
+      description='',
+      url='https://github.com/brodderickrodriguez/strategy_learning_system',
+      author='Brodderick Rodriguez',
+      author_email='bcr@brodderick.com',
+      license='MIT',
+      packages=['strategy_learning_system'],
+      zip_safe=False)
---
+++ strategy_learning_system/__init__.py
@@ -0,0 +1,10 @@
+# Brodderick Rodriguez
+# Auburn University - CSSE
+# 27 Aug. 2019
+
+
+from .context import Context
+from .interpreter import Interpreter
+from . import model_executor 
+from . import learn
+from . import explain
