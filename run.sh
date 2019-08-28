9aaa8e773f7b8331ac514c7631bfe2199a857392
brodderickrodriguez <bcr@brodderick.com>
2019-08-28 17:24:31 -0500

    updated run.sh

--- run.sh
+++ run.sh
@@ -1,6 +1,10 @@
 
- echo "y" | pip uninstall strategy_learning_system
+echo "y" | pip3 uninstall strategy_learning_system
 
- pip install .
+pip3 install .
 
- python3 case_studies/prey_pred_model/prey_pred_model.py
+clear
+
+python3 case_studies/prey_pred_model/prey_pred_model.py
+
+rm -rf tmp*
