b83da25865e2e4e0d1010ecb4ce8ad416b9403f3
brodderickrodriguez <bcr@brodderick.com>
2019-09-03 12:28:01 -0500

    working on getting ema to work

--- strategy_learning_system/model_synthesizer.py
+++ strategy_learning_system/model_synthesizer.py
@@ -11,29 +11,34 @@ from ema_workbench import ema_logging, MultiprocessingEvaluator
 NETLOGO_HOME = '/home/bcr/apps/netlogo'
 
 
-def _build_executable_model(model_info, resolution_model, max_run_length, num_repititions):
-	ema_model = NetLogoModel(model_info['inter_name'], 
-							wd=model_info['dir'], 
-							model_file=model_info['name'], 
-							netlogo_home=NETLOGO_HOME, 
-							netlogo_version=6, 
-							gui=False)
+def _build_executable_model(nediator, context):
+	pass
+	# ema_model = NetLogoModel(model_info['inter_name'], 
+	# 						wd=model_info['dir'], 
+	# 						model_file=model_info['name'], 
+	# 						netlogo_home=NETLOGO_HOME, 
+	# 						netlogo_version=6, 
+	# 						gui=False)
 
-	ema_model.max_run_length = max_run_length
-	ema_model.replications = num_repititions
+	# ema_model.max_run_length = max_run_length
+	# ema_model.replications = num_repititions
 
-	uncertainties, outcomes = resolution_model
-	ema_model.uncertainties = uncertainties
-	ema_model.outcomes = outcomes
+	# uncertainties, outcomes = resolution_model
+	# ema_model.uncertainties = uncertainties
+	# ema_model.outcomes = outcomes
 
-	return ema_model
+	# return ema_model
 
-
-def execute(model_info, resolution_model, num_experiments, max_run_length, num_repititions, num_processes):
+#model_info, resolution_model, num_experiments, max_run_length, num_repititions, num_processes
+def synthesize(mediator, context):
 	# ema_logging.log_to_stderr(ema_logging.INFO)
-	ema_model = _build_executable_model(model_info, resolution_model, max_run_length, num_repititions)
+	ema_model = _build_executable_model(mediator, context)
 
-	with MultiprocessingEvaluator(ema_model, n_processes=num_processes, maxtasksperchild=4) as evaluator:
-		results = evaluator.perform_experiments(num_experiments)
+	return None
+
+	with MultiprocessingEvaluator(ema_model, 
+								n_processes=context.num_processes, 
+								maxtasksperchild=4) as evaluator:
+		results = evaluator.perform_experiments(context.num_experiments)
 
 	return results
