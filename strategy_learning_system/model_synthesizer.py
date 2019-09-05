ae48a6f0910d9d53993fc8ce26b981e913406714
brodderickrodriguez <bcr@brodderick.com>
2019-09-05 10:51:24 -0500

    added comments

--- strategy_learning_system/model_synthesizer.py
+++ strategy_learning_system/model_synthesizer.py
@@ -12,25 +12,44 @@ NETLOGO_HOME = '/home/bcr/apps/netlogo'
 
 
 def _build_executable_model(mediator, context):
-	name = mediator.name
-	model_dir_path = mediator.model['dir']
-	model_file = mediator.model['name']
-	netlogo_home = NETLOGO_HOME
-	netlogo_version = 6
-	gui = False
-
+	# define lists for the model's uncertainties and outcomes
 	uncertainties, outcomes = [], []
 
+	# separate uncertainties and outcomes for EMA 
 	for attr in context.resolution_model:
 		if attr in mediator.feature_model.outcomes:
 			outcomes.append(attr)
 		else:
 			uncertainties.append(attr)
 
-	ema_model = NetLogoModel(name, model_dir_path, model_file, netlogo_home, netlogo_version, gui)
-	ema_model.run_length = context.max_run_length
-	ema_model.replications = context.num_repititions
+	# set the name of the EMAModel
+	name = mediator.name
 
+	# set the dir path for the model
+	model_dir_path = mediator.model['dir']
+
+	# set the name of the model
+	model_file = mediator.model['name']
+
+	# set the dir path where NetLogo is located
+	netlogo_home = NETLOGO_HOME
+
+	# set the NetLogo version
+	netlogo_version = 6
+
+	# turn GUI off
+	gui = False
+
+	# create the NetLogoModel object
+	ema_model = NetLogoModel(name, model_dir_path, model_file, netlogo_home, netlogo_version, gui)
+	
+	# set the max run length of this model
+	ema_model.run_length = context.max_run_length
+
+	# set the number of replications
+	ema_model.replications = context.num_replications
+
+	# set the model uncertainties and outcomes
 	ema_model.uncertainties = uncertainties
 	ema_model.outcomes = outcomes
 
@@ -38,14 +57,13 @@ def _build_executable_model(mediator, context):
 
 
 def synthesize(mediator, context):
-	# ema_logging.log_to_stderr(ema_logging.INFO)
+	# turn logging on
+	ema_logging.log_to_stderr(ema_logging.INFO)
+
+	# create a EMA Model Object
 	ema_model = _build_executable_model(mediator, context)
 
 	print(ema_model.outcomes)
-
-
-	for o in ema_model.uncertainties:
-		print(o)
 
 	return None
 
