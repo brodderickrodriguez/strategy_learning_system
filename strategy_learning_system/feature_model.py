449dc0ae6238facd4e053e625639d3a23b00baab
brodderickrodriguez <bcr@brodderick.com>
2019-12-17 13:03:04 -0600

    Update feature_model.py

--- strategy_learning_system/feature_model.py
+++ strategy_learning_system/feature_model.py
@@ -5,21 +5,54 @@
 import ema_workbench
 import copy
 
-IntegerParameter = ema_workbench.IntegerParameter
-TimeSeriesOutcome = ema_workbench.TimeSeriesOutcome
+
+def _compare_params(a, b):
+	def _get_name(c):
+		if hasattr(c, 'name'):
+			return c.name
+		else:
+			return c
+
+	return _get_name(a) == _get_name(b)
+
+
+class IntegerParameter(ema_workbench.IntegerParameter):
+	def __init__(self, *args, **kwargs): super().__init__(*args, **kwargs)
+	def __eq__(self, other): return _compare_params(self, other)
+
+
+class RealParameter(ema_workbench.RealParameter):
+	def __init__(self, *args, **kwargs): super().__init__(*args, **kwargs)
+	def __eq__(self, other): return _compare_params(self, other)
+
+
+class CategoricalParameter(ema_workbench.CategoricalParameter):
+	def __init__(self, *args, **kwargs):
+		super().__init__(*args, **kwargs)
+		self.index_to_category = {i: v for i, v in enumerate(self._categories._data.keys())}
+		self.category_to_index = {v: i for i, v in self.index_to_category.items()}
+
+	def __eq__(self, other):
+		return _compare_params(self, other)
+
+
+class TimeSeriesOutcome(ema_workbench.TimeSeriesOutcome):
+	def __init__(self, *args, **kwargs): super().__init__(*args, **kwargs)
+	def __eq__(self, other): return _compare_params(self, other)
 
 
 class FeatureModel:
 	def __init__(self):
-		self.environmental_uncertainties = []
-		self.model_uncertainties = []
-		self.outcomes = []
+		pass
 
 	def __getitem__(self, key):
 		params = {e.name: e for e in self.__iter__()}
 
 		if key in params:
 			return params[key]
+
+	def __str__(self):
+		return str(list(self.__iter__()))
 
 	def __iter__(self):
 		_all = (self.model_uncertainties + self.environmental_uncertainties + self.outcomes)
