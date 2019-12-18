4af66439216c73b7416bf3f13ab2c1d9d6cb75bc
brodderickrodriguez <bcr@brodderick.com>
2019-12-18 14:52:56 -0600

    Update model_synthesizer.py

--- strategy_learning_system/model_synthesizer.py
+++ strategy_learning_system/model_synthesizer.py
@@ -8,17 +8,50 @@ from ema_workbench.connectors.netlogo import NetLogoModel
 from ema_workbench import ema_logging, MultiprocessingEvaluator
 
 
+def _determine_parameters(mediator, context):
+	resolution = set([fi for sft in context.resolution_model for fi in sft.collapse(ema_objects_only=False)])
+	all_features = set(mediator.feature_model.collapse())
+	difference = all_features.difference(resolution)
+	move_difference_to_resolution = []
+
+	for i, fi in enumerate(difference):
+		if isinstance(fi, ema_workbench.CategoricalParameter):
+			cats = {c.name: 0 for c in fi.categories}
+
+			for j, fj in enumerate(resolution):
+				if fj.name in cats:
+					cats[fj.name] += 1
+
+			cat_occurrences = sum(cats.values())
+			if cat_occurrences != 1:
+				move_difference_to_resolution.append(fi)
+			else:
+				for k, v in cats.items():
+					if v == 1:
+						fi.default = k
+						break
+
+	for f in move_difference_to_resolution:
+		difference.remove(f)
+		resolution.add(f)
+
+	def is_outcome(f):
+		return isinstance(f, ema_workbench.TimeSeriesOutcome) or \
+			   isinstance(f, ema_workbench.ArrayOutcome) or \
+			   isinstance(f, ema_workbench.ScalarOutcome)
+
+	def is_ema_object(f):
+		return hasattr(f, 'is_ema_object')
+
+	constants = [f.to_constant() for f in difference if not is_outcome(f) and is_ema_object(f)]
+	resolution_uncertainties = [f for f in resolution if not is_outcome(f) and is_ema_object(f)]
+	resolution_outcomes = [f for f in resolution if is_outcome(f) and is_ema_object(f)]
+
+	context.resolution_model = constants + resolution_uncertainties + resolution_outcomes
+	return resolution_uncertainties, resolution_outcomes, constants
+
+
 def _build_executable_model(mediator, context):
-	# define lists for the model's uncertainties and outcomes
-	uncertainties, outcomes = [], []
-
-	# separate uncertainties and outcomes for EMA 
-	for attr in context.resolution_model:
-		if attr in mediator.feature_model.outcomes:
-			outcomes.append(attr)
-		else:
-			uncertainties.append(attr)
-
 	# set the name of the EMAModel
 	name = mediator.name
 
@@ -51,10 +84,10 @@ def _build_executable_model(mediator, context):
 	# set the number of replications
 	ema_model.replications = context.num_replications
 
-	# set the model uncertainties and outcomes
+	uncertainties, outcomes, constants = _determine_parameters(mediator, context)
 	ema_model.uncertainties = uncertainties
 	ema_model.outcomes = outcomes
-
+	ema_model.constants = constants
 	return ema_model
 
 
