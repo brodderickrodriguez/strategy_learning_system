d765e185a60fdc4dfea0a442a4d736679192299b
brodderickrodriguez <bcr@brodderick.com>
2019-12-17 19:02:38 -0600

    Update plume_model.py

--- case_studies/plume_model/plume_model.py
+++ case_studies/plume_model/plume_model.py
@@ -22,64 +22,102 @@ SAVE_LOC = ROOT + '/data/sls_data'
 
 
 def define_feature_model():
-	# environmental uncertainties
-	num_plumes = sls.IntegerParameter('number-plumes', 0, 5)
+	# environment territory
+	world_edge_threshold = sls.RealParameter('world-edge-threshold', 0, 25)
+	max_world_edge_turn = sls.RealParameter('max-world-edge-turn', 0, 20)
+	territory = sls.Feature(feature_name='territory')
+	territory.add_sub_feature(world_edge_threshold, sls.FeatureType.model, sls.Constraint.optional)
+	territory.add_sub_feature(max_world_edge_turn, sls.FeatureType.model, sls.Constraint.optional)
+
+	# environment wind
+	wind_speed = sls.RealParameter('wind-speed', 0, 0.1)
+	wind_heading = sls.IntegerParameter('wind-heading', 0, 360)
+	wind = sls.Feature(feature_name='wind')
+	wind.add_sub_feature(wind_speed, sls.FeatureType.environmental, sls.Constraint.mandatory)
+	wind.add_sub_feature(wind_heading, sls.FeatureType.environmental, sls.Constraint.mandatory)
+
+	# environment plume characteristics
 	plume_spread = sls.RealParameter('plume-spread-radius', 0, 1)
 	plume_decay = sls.RealParameter('plume-decay-rate', 0, 1e-4)
 	plume_decon_threshold = sls.RealParameter('plume-decontamination-threshold', 0.01, 1)
-	wind_speed = sls.RealParameter('wind-speed', 0, 0.1)
-	wind_heading = sls.IntegerParameter('wind-heading', 0, 360)
-	coverage_data_decay = sls.IntegerParameter('coverage-data-decay', 1, 60)
+	plume_characteristics = sls.Feature(feature_name='plume characteristics')
+	plume_characteristics.add_sub_feature(plume_spread, sls.FeatureType.environmental, sls.Constraint.mandatory)
+	plume_characteristics.add_sub_feature(plume_decay, sls.FeatureType.environmental, sls.Constraint.optional)
+	plume_characteristics.add_sub_feature(plume_decon_threshold, sls.FeatureType.environmental, sls.Constraint.optional)
 
-	# model uncertainties
-	pop = sls.IntegerParameter('population', 2, 100)
+	# environment
+	environment = sls.Feature(feature_name='environment')
+	num_plumes = sls.IntegerParameter('number-plumes', 0, 5)
+	environment.add_sub_feature(num_plumes, sls.FeatureType.environmental, sls.Constraint.mandatory)
+	environment.add_sub_feature(territory, sls.FeatureType.environmental, sls.Constraint.optional)
+	environment.add_sub_feature(wind, sls.FeatureType.environmental, sls.Constraint.optional)
+	environment.add_sub_feature(plume_characteristics, sls.FeatureType.environmental, sls.Constraint.mandatory)
+
+	# swarm UAV Capacity sensor reading
+	coverage_data_decay = sls.IntegerParameter('coverage-data-decay', 1, 60)
+	coverage_per = sls.TimeSeriesOutcome('coverage-percentage')
+	coverage_std = sls.TimeSeriesOutcome('coverage-std')
+	coverage_mean = sls.TimeSeriesOutcome('coverage-mean')
+	sensor_reading = sls.Feature(feature_name='sensor reading')
+	sensor_reading.add_sub_feature(coverage_data_decay, sls.FeatureType.model, sls.Constraint.optional)
+	sensor_reading.add_sub_feature(coverage_per, sls.FeatureType.outcome, sls.Constraint.optional)
+	sensor_reading.add_sub_feature(coverage_std, sls.FeatureType.outcome, sls.Constraint.optional)
+	sensor_reading.add_sub_feature(coverage_mean, sls.FeatureType.outcome, sls.Constraint.optional)
+
+	# swarm uav capacity
 	uav_vision = sls.RealParameter('UAV-vision', 0, 195)
 	uav_decon_strength = sls.RealParameter('UAV-decontamination-strength', 0, 0.01)
-	world_edge_threshold = sls.RealParameter('world-edge-threshold', 0, 25)
-	max_world_edge_turn = sls.RealParameter('max-world-edge-turn', 0, 20)
+	uav_capacity = sls.Feature(feature_name='uav capacity')
+	uav_capacity.add_sub_feature(sensor_reading, sls.FeatureType.outcome, sls.Constraint.mandatory)
+	uav_capacity.add_sub_feature(uav_vision, sls.FeatureType.environmental, sls.Constraint.optional)
+	uav_capacity.add_sub_feature(uav_decon_strength, sls.FeatureType.environmental, sls.Constraint.optional)
 
-	search_policy = sls.CategoricalParameter('global-search-policy',
-	categories=('\"random-search\"', '\"flock-search\"', '\"symmetric-search\"'))
-
-	# flocking policy
+	# swarm search behavior flock policy
 	min_sep = sls.RealParameter('minimum-separation', 0, 5)
 	max_align = sls.RealParameter('max-align-turn', 0, 20)
 	max_cohere = sls.RealParameter('max-cohere-turn', 0, 10)
 	max_separate = sls.RealParameter('max-separate-turn', 0, 20)
+	flock_policy = sls.Feature(feature_name='flock search policy')
+	flock_policy.add_sub_feature(min_sep, sls.FeatureType.model, sls.Constraint.mandatory)
+	flock_policy.add_sub_feature(max_align, sls.FeatureType.model, sls.Constraint.mandatory)
+	flock_policy.add_sub_feature(max_cohere, sls.FeatureType.model, sls.Constraint.mandatory)
+	flock_policy.add_sub_feature(max_separate, sls.FeatureType.model, sls.Constraint.mandatory)
 
-	# random policy
+	# swarm search behavior random policy
 	rand_max_heading_time = sls.IntegerParameter('random-search-max-heading-time', 0, 100)
 	rand_max_turn = sls.RealParameter('random-search-max-turn', 0, 5)
+	random_policy = sls.Feature(feature_name='random search policy')
+	random_policy.add_sub_feature(rand_max_heading_time, sls.FeatureType.model, sls.Constraint.mandatory)
+	random_policy.add_sub_feature(rand_max_turn, sls.FeatureType.model, sls.Constraint.mandatory)
 
-	# symmetric policy
+	# swarm search behavior symmetric policy
 	sym_max_turn = sls.RealParameter('symmetric-search-max-turn', 0, 20)
 	sym_region_threshold = sls.RealParameter('symmetric-search-region-threshold', -10, 25)
 	sym_min_region_time = sls.IntegerParameter('symmetric-search-min-region-time', 1, int(1e3))
 	sym_max_region_time = sls.IntegerParameter('symmetric-search-max-region-time', int(1e2), int(5e3))
+	symmetric_policy = sls.Feature(feature_name='symmetric search policy')
+	symmetric_policy.add_sub_feature(sym_max_turn, sls.FeatureType.model, sls.Constraint.mandatory)
+	symmetric_policy.add_sub_feature(sym_region_threshold, sls.FeatureType.model, sls.Constraint.mandatory)
+	symmetric_policy.add_sub_feature(sym_min_region_time, sls.FeatureType.model, sls.Constraint.mandatory)
+	symmetric_policy.add_sub_feature(sym_max_region_time, sls.FeatureType.model, sls.Constraint.mandatory)
 
-	# outcomes
-	coverage_per = sls.TimeSeriesOutcome('coverage-percentage')
-	coverage_std = sls.TimeSeriesOutcome('coverage-std')
-	coverage_mean = sls.TimeSeriesOutcome('coverage-mean')
+	#swarm search behavior
+	search_behavior = sls.Feature(netlogo_categorical='global-search-policy', feature_name='search behavior')
+	search_behavior.add_sub_feature(flock_policy, sls.FeatureType.model, sls.Constraint.xor, '\"flock-search\"')
+	search_behavior.add_sub_feature(random_policy, sls.FeatureType.model, sls.Constraint.xor, '\"random-search\"')
+	search_behavior.add_sub_feature(symmetric_policy, sls.FeatureType.model, sls.Constraint.xor, '\"flock-search\"')
 
-	environmental_uncertainties = [num_plumes, plume_spread, plume_decay,
-									plume_decon_threshold, wind_speed,
-									wind_heading, coverage_data_decay]
+	# swarm
+	pop = sls.IntegerParameter('population', 2, 100)
+	swarm = sls.Feature(feature_name='swarm')
+	swarm.add_sub_feature(uav_capacity, sls.FeatureType.environmental, sls.Constraint.mandatory)
+	swarm.add_sub_feature(pop, sls.FeatureType.model, sls.Constraint.mandatory)
+	swarm.add_sub_feature(search_behavior, sls.FeatureType.model, sls.Constraint.mandatory)
 
-	model_uncertainties = [search_policy, pop, uav_vision, uav_decon_strength,
-							world_edge_threshold, max_world_edge_turn,
-							min_sep, max_align, max_cohere,
-							max_separate, rand_max_heading_time, rand_max_turn,
-							sym_max_turn, sym_region_threshold,
-							sym_min_region_time, sym_max_region_time]
-
-	outcomes = [coverage_per, coverage_std, coverage_mean]
-
+	# feature model
 	feature_model = sls.FeatureModel()
-	feature_model.environmental_uncertainties = environmental_uncertainties
-	feature_model.model_uncertainties = model_uncertainties
-	feature_model.outcomes = outcomes
-
+	feature_model.add_sub_feature(environment, sls.FeatureType.environmental, sls.Constraint.mandatory)
+	feature_model.add_sub_feature(swarm, sls.FeatureType.environmental, sls.Constraint.mandatory)
 	return feature_model
 
 
@@ -117,7 +155,7 @@ def create_context1(mediator):
 	cxt.reward_function = reward_function_1
 	cxt.resolution_model = cxt1_resolution
 	# print(list(mediator.feature_model))
-	print(cxt1_resolution)
+	# print(cxt1_resolution)
 
 	cxt.bins = np.linspace(0.0, 1.0, 3)
 	cxt.num_experiments = 5
@@ -129,14 +167,23 @@ def create_context1(mediator):
 
 
 med = create()
-cxt1 = create_context1(med)
-med.evaluate_context(cxt1)
-med.save()
+
+print(med.feature_model)
+
+env = med.feature_model['search behavior']
+
+print(med.feature_model.subtree_str(env))
+
+print(med.feature_model.collapse(env))
+
+# cxt1 = create_context1(med)
+# med.evaluate_context(cxt1)
+# med.save()
 
 #
 # med = sls.ModelMediator.load('{}/{}'.format(SAVE_LOC, MEDIATOR_NAME))
-cxt1 = med['context1']
-print(cxt1.processed_exploratory_results)
+# cxt1 = med['context1']
+# print(cxt1.processed_exploratory_results)
 
 # print(med.feature_model['global-search-policy']._categories)
 
