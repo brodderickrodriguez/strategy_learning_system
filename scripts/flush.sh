8e5b8b85bc0cec29087d2d11659d48ef3a84a588
brodderickrodriguez <bcr@brodderick.com>
2019-12-24 12:06:48 -0600

    update requirements and bash scripts

--- scripts/flush.sh
+++ scripts/flush.sh
@@ -1,3 +1,4 @@
+#!/usr/bin/env bash
 echo "y" | pip3 uninstall strategy_learning_system
 
 pip3 install ../.
---
+++ scripts/run_local.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+echo "y" | pip3 uninstall xcsr
+
+pip3 install git+/Users/bcr/Dropbox/projects/code/python/xcsr
+
+clear
+
+echo "flushed"
