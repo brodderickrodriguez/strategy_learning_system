47cc62357129f40ada2f25b487345cd417556b2b
brodderickrodriguez <bcr@brodderick.com>
2019-12-23 22:26:45 -0600

    added exploratory plotting

--- strategy_learning_system/explain.py
+++ strategy_learning_system/explain.py
@@ -1,59 +1,55 @@
 # Brodderick Rodriguez
 # Auburn University - CSSE
-# 24 Aug. 2019
+# 24 Dec. 2019
 
 
-# TODO: HEAT MAPS ETC
 import seaborn as sns
 import numpy as np
 import matplotlib.pyplot as plt
-import copy
 import itertools
+import pandas as pd
 
-def plot_explored(mediator, context):
+
+def plot_explored(context):
 	sns.set()
-
 	# axis0: y - environmental
 	# axis1: x - model
 
 	environmental = [f.name for f in context.environmental_uncertainties()]
 	model = [f.name for f in context.model_uncertainties()]
 
-	data = copy.deepcopy(context.processed_exploratory_results)
-
-
-	# print(data[environmental])
+	data = context.processed_exploratory_results
 
 	x_values = list(itertools.product(range(context.bins.shape[0]), repeat=len(model)))
 	y_values = list(itertools.product(range(context.bins.shape[0]), repeat=len(environmental)))
 
-	print(x_values)
+	x_values = [''.join(str(xi) for xi in x) for x in x_values]
+	y_values = [''.join(str(yi) for yi in y) for y in y_values]
 
+	x_values[0] = 'a'
 
+	x_dict = {x: [] for x in x_values}
+	structured_data = {y: x_dict for y in y_values[::-1]}
 
+	def row_to_string(row, uncertainties):
+		n = [int(i) for i in row[uncertainties]]
+		n_idx = ''.join(str(i) for i in n)
+		return n_idx
 
-	# print(environmental)
+	for i in range(data.shape[0]):
+		row = data.iloc[i]
+		env_idx = row_to_string(row, environmental)
+		mod_idx = row_to_string(row, model)
+		structured_data[env_idx][mod_idx].append(row['rho'])
 
-	print(context.bins.shape[0])
+	for y_key in structured_data:
+		y_i = structured_data[y_key]
+		for x_key in y_i:
+			y_i[x_key] = np.mean(y_i[x_key])
 
-
-	exit()
-
-	# print(context.processed_exploratory_results)
-
-
-	# Load the example flights dataset and conver to long-form
-	flights_long = sns.load_dataset("flights")
-	flights = flights_long.pivot("month", "year", "passengers")
-
-	print(type(flights))
-
-	print(flights)
-
-	# Draw a heatmap with the numeric values in each cell
-	f, ax = plt.subplots(figsize=(9, 6))
-	sns.heatmap(context.processed_exploratory_results, annot=False, fmt="f", linewidths=.5, ax=ax)
-
+	data = pd.DataFrame.from_dict(structured_data, orient='index')
+	f, ax = plt.subplots(figsize=(12, 10))
+	sns.heatmap(data, annot=False, fmt="f", linewidths=.5, ax=ax)
 	plt.show()
 
 
